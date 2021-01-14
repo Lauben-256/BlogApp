496fca1429d171588807c04e1b5a36c6bd5bf3cc
Tumusiime Lauben Junior <tumusiimelauben9@gmail.com>
2021-01-14 19:47:00 +0800

    Newest files

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    # Home page
+    path('', views.home, name = 'blog-home'),
+    path('about/', views.about, name = 'blog-about'),
+]
