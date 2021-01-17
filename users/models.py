7a05734999fa637774294985e3b8e3395f7d99b1
Tumusiime Lauben Junior <tumusiimelauben9@gmail.com>
2021-01-17 18:28:13 +0800

    Resize and update profiles

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import  Image
 
 # Create your models here.
 
@@ -9,3 +10,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self): # This is the method that gets run after our model is saved.
+        super().save() # To run the save method 
+
+        # Get the image it saved and resize it
+        img = Image.open(self.image.path) # Open the image of the current instance.
+
+        # First, check if the image is more than 300px
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path) # Save the image to the same to override the oversize image
