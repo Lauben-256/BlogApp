76bfd619a09c10496fdcfba51d66e2eb711b5812
Tumusiime Lauben Junior <tumusiimelauben9@gmail.com>
2021-01-22 21:11:13 +0800

    Paginated the pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from .views import (
     PostDetailView, 
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView,
     )
 
 from . import views
@@ -12,7 +13,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     # Home page
-    path('', PostListView.as_view(), name = 'blog-home'),
+    path('', views.home, name = 'blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name = 'user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
